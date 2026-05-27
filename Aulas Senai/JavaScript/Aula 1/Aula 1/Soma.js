c8e2d47950270b168d45896dbccc55f3e19d1739
Davi Profeta <davihpg2008@gmail.com>
2026-05-27 11:10:10 -0300

    Add files via upload

---
+++ Aulas Senai/JavaScript/Aula 1/Aula 1/Soma.js
@@ -0,0 +1,4 @@
+let num1 = Number(4);
+let num2 = Number(4);
+let soma = Number(num1+num2)
+console.log("O valor da soma é " + soma);
---
+++ Aulas Senai/JavaScript/Aula 1/Aula 1/se-snão.js
@@ -0,0 +1,8 @@
+let num1 = Number(10);
+let num2 = Number(30);
+
+if(num1 > num2){
+    console.log("O numero " +num1+ " é maior que o "+num2)
+} else{
+    console.log("O numero " +num2+ " é maior que o "+num1)
+}
