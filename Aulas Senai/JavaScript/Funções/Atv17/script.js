f98b15136be2abe5ee9054d101a659a7de214fb4
Davi Profeta <davihpg2008@gmail.com>
2026-06-11 08:47:06 -0300

    Add files via upload

---
+++ Aulas Senai/JavaScript/Funções/Atv17/script.js
@@ -0,0 +1,11 @@
+/* Crie uma função que receba um número inteiro n e imprima todos os números de 1
+até n, usando for. */
+function caminho(num){
+
+    for(let i=1; i<=num ; i++){
+        console.log(i)
+    }
+
+}
+
+caminho(10)
